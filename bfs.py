b9a071ebdc3ecca8b30cd6000fcfb8e7c13d8833
altaa <1617328+tbtraltaa@users.noreply.github.com>
2017-11-15 03:52:07 -0800

    Complete

--- bfs.py
+++ bfs.py
@@ -5,7 +5,6 @@
 # 
 # 
 
-# In[20]:
 
 
 #get_ipython().magic(u'matplotlib inline')
@@ -13,22 +12,102 @@ import warnings
 warnings.filterwarnings('ignore')
 import networkx as nx
 import matplotlib.pyplot as plt
+import numpy as np
 
+def adjacency_list(FStar):
+    '''
+    Outarc adjacency list
+    '''
+    A = dict()
+    N = np.sort(np.unique(FStar.reshape(-1,)))
+    for i in N:
+        A[i] = FStar[:,1][np.argwhere(FStar[:,0]==i)].reshape(-1,).tolist()
+    return A
+
+def bfs(FStar, s, n=None):
+    '''
+    Breadth First Search using Forward Star representation.
+    FStar - Forward Star representation
+    s - source node
+    '''
+    A = adjacency_list(FStar)
+    # Number of nodes
+    if n is None:
+        n = len(A)
+    # Tails of arcs
+    T = FStar[:,0]
+    H = FStar[:,1]
+    # Heads of arcs
+    LIST = list([s])
+    mark = np.zeros(n)
+    next_= 1
+    mark[s] = 1
+    parent = np.zeros(n)
+    parent[s] = 0
+    order = np.zeros(n)
+    order[s] = next_
+    while len(LIST) != 0:
+        i = LIST.pop(0)
+        # Unmarked j nodes of (i,j) arcs 
+        jj = []
+        if i in A.keys():
+            if A[i] != []:
+                jj = [j for j in A[i] if mark[j]==0]
+                # Lexicographical ordering
+                jj.sort()
+            if len(jj) !=0:
+                for j in jj:
+                    mark[j] = 1
+                    parent[j] = i
+                    next_ += 1
+                    order[j] = next_
+                    LIST.append(j)
+        # Draw the traversal steps
+        G = nx.DiGraph()
+        G.add_nodes_from([1,2,3,4,5,6])
+        G.add_edges_from([(1,2),(1,3),(2,3),(2,4),(2,5),(3,4),(4,6),(5,4),(5,6)])
+        shells=[[6,5,2,1,3,4]]
+        node_color = ["b" if mark[i]==1 else "r" for i in range(1,7)]
+        nx.draw_networkx(G, pos=nx.shell_layout(G, shells), node_color=node_color, font_weight='bold')
+        ax = plt.gca()
+        plt.axis("off")
+        plt.title(str(LIST))
+        plt.show()
+    index = np.arange(1, n+1, 1).reshape(-1,1)
+  
+    return order, parent
+#Drawing the given graph
 G = nx.DiGraph()
 G.add_nodes_from([1,2,3,4,5,6])
 G.add_edges_from([(1,2),(1,3),(2,3),(2,4),(2,5),(3,4),(4,6),(5,4),(5,6)])
-plt.subplot(121)
 shells=[[6,5,2,1,3,4]]
+plt.title("[1]")
 nx.draw_shell(G, nlist=shells, with_labels=True, font_weight='bold')
+plt.show()
+
+FStar = np.array([[0, 0],
+                    [1,2],
+                    [1,3],
+                    [2,3],
+                    [2,4],
+                    [2,5],
+                    [3,4],
+                    [4,6],
+                    [5,4],
+                    [5,6],])
+#BFS
+order, parent = bfs(FStar,1)
+#BFS search tree
 T = nx.bfs_tree(G, source=1)
-plt.subplot(122)
+plt.title("Order: " + str(order[1:]) + " , " + "Parent: " + str(parent[1:]))
 nx.draw_shell(T, nlist=shells, with_labels=True, font_weight='bold')
 plt.show()
 
 
+
 # ## Linear Algebraic approach to Breadth First Search
 
-# In[32]:
+# In[55]:
 
 '''
 from IPython.display import display, Javascript
@@ -41,9 +120,10 @@ IPython.OutputArea.prototype._should_scroll = function(lines) {
 
 display(Javascript(disable_js))
 get_ipython().magic(u'matplotlib inline')
+'''
 import warnings
 warnings.filterwarnings('ignore')
-'''
+
 import networkx as nx
 import matplotlib.pyplot as plt
 import numpy as np
@@ -60,14 +140,20 @@ A_t = A.transpose().copy()
 x = np.zeros((6,1), dtype=int,)
 x[0] = 1
 t = 1
+node_color = ["b" if i in x.nonzero()[0] else "r" for i in range(6)]
+nx.draw_networkx(G, pos=nx.shell_layout(G, shells), node_color=node_color, font_weight='bold')
+ax = plt.gca()
+plt.axis("off")
+plt.title('x='+ str(x.reshape(-1,)))
+plt.show()
+    
 while True:
     y = A_t*x
-    print "x=", x.reshape(-1,)
-    node_color = ["b" if i in x.nonzero()[0] else "r" for i in range(6)]
+    node_color = ["b" if i in y.nonzero()[0] else "r" for i in range(6)]
     nx.draw_networkx(G, pos=nx.shell_layout(G, shells), node_color=node_color, font_weight='bold')
     ax = plt.gca()
     plt.axis("off")
-    plt.title(str(x.reshape(-1,)))
+    plt.title(r'$(A^{T})^{%d}x=$%s$^{T}$'%(t,str(y.reshape(-1,))))
     plt.show()
     t += 1
     if np.array_equal(x,y):
@@ -76,6 +162,12 @@ while True:
        x = y
     else:
         break
-    
+
         
+
+
+# In[ ]:
+
+
+
 
--- shortest-path.py
+++ shortest-path.py
@@ -3,7 +3,7 @@
 
 # ## Shortest Path Algorithm
 
-# In[1]:
+# In[19]:
 
 '''
 from IPython.display import display, Javascript
@@ -36,9 +36,9 @@ def adjacency_list(FStar):
         A[i] = FStar[:,1][np.argwhere(FStar[:,0]==i)].reshape(-1,).tolist()
     return A
 
-def FIFOlc(FStar, s, d=None):
+def FIFO_ShortestPath(FStar, s, d=None):
     '''
-    FIFO label correcting algorithm
+    FIFO Shortest Path Algorithm
     '''
     A = adjacency_list(FStar)
     n = len(A)
@@ -47,15 +47,15 @@ def FIFOlc(FStar, s, d=None):
         d = np.ndarray((n+1,))
         d[:] = np.inf
         d[s] = 0
-    pred = np.zeros(n+1, dtype=int)
-    pred[s] = 0
+    parent = np.zeros(n+1, dtype=int)
+    parent[s] = 0
     while len(NODELIST) != 0:
         i = NODELIST.pop(0) #Removes the first element
         for j in A[i]:
             c = FStar[np.all(FStar[:,0:2]==[i,j], axis=1),2]
             if d[j] > d[i] + c:
                 d[j] = d[i] + c
-                pred[j] = i
+                parent[j] = i
                 if j not in NODELIST:
                     NODELIST.append(j)
         # Drawing each pass
@@ -64,6 +64,7 @@ def FIFOlc(FStar, s, d=None):
         G.add_weighted_edges_from([(1,2, 3),(1,3, 2),(2,3,2),(2,4,2),(2,5,4),(3,4,5),(4,6,3),(5,4,2),(5,6,2)])
         shells=[[6,5,2,1,3,4]]
         pos = nx.shell_layout(G, shells)
+        plt.title(str(NODELIST))
         nx.draw(G,pos=pos, with_labels=False, font_weight='bold', node_size=2000)
         labels = nx.get_edge_attributes(G,'weight')
         nx.draw_networkx_edge_labels(G,pos=pos,edge_labels=labels)
@@ -72,7 +73,7 @@ def FIFOlc(FStar, s, d=None):
         #    pos[p][1] += offset
         nx.draw_networkx_labels(G, pos, labels={l:"%d, %s"%(l,d[l]) for l in range(1,7)})
         plt.show()
-    return pred, d
+    return parent, d
 
 # Forward star representation [T, H, C, U]
 # [0, 0, 0] is a dummy node.
@@ -97,7 +98,7 @@ labels = nx.get_edge_attributes(G,'weight')
 nx.draw_networkx_edge_labels(G,pos=pos,edge_labels=labels)
 plt.show()
 
-pred, d = FIFOlc(FStar, 1)
+parent, d = FIFO_ShortestPath(FStar, 1)
 
 #Draw the shortest path tree
 G = nx.DiGraph()
@@ -105,6 +106,7 @@ G.add_nodes_from([1,2,3,4,5,6])
 G.add_weighted_edges_from([(1,2, 3),(1,3, 2),(2,4,2),(2,5,4),(4,6,3)])
 shells=[[6,5,2,1,3,4]]
 pos = nx.shell_layout(G, shells)
+plt.title("Parent = " + str(parent[1:]) + " , Distance, d = " + str(d[1:]))
 nx.draw(G,pos=pos, with_labels=False, font_weight='bold', node_size=2000)
 labels = nx.get_edge_attributes(G,'weight')
 nx.draw_networkx_edge_labels(G,pos=pos,edge_labels=labels)
